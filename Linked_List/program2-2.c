90dca126a0e9b7dfe4083590a97aba2cbf3d1fe3
Sushantchaudhary1 <119941898+Sushantchaudhary1@users.noreply.github.com>
2023-04-15 21:58:37 +0530

    Add files via upload

---
+++ Linked_List/program2-2.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+  int info;
+  struct node *next;
+};
+
+struct node *head=NULL;
+void create(struct node *);
+void display(struct node *);
+
+int main(){
+ int ch;
+ while(1)
+ {
+    printf("\nPress 1-Create a Node 2-Display a Node 0-Exit\n");
+    printf("Enter your choice:");
+    scanf("%d",&ch);
+
+    switch(ch)
+    {
+        case 1:
+             create(head);
+             break;
+
+        case 2:
+            display(head);
+            break;
+
+        case 0:
+            exit(1);
+            break;
+            default:
+            printf("Wrong choice:");
+
+    }
+ }
+    return 0;
+}
+void create(struct node *ptr)
+{
+    struct node *temp;
+    int data;
+
+    temp=(struct node *)malloc(sizeof(struct node));
+    printf("Enter the Data:");
+    scanf("%d",&data);
+    temp->info=data;
+    temp->next=NULL;
+    if(head==NULL)
+    {
+        head=temp;
+    }
+    else{
+        ptr=head;
+        while(ptr->next!=NULL){
+            ptr=ptr->next;
+        }
+         ptr->next=temp;
+    }
+    printf("Node created Successfully.....");
+}
+void display(struct node *ptr)
+{
+    if(head==NULL){
+     printf("Empty Linked List.....");
+    }
+     else{
+ 
+     printf("\nLinked list is:\n");
+    while(ptr!=NULL){
+        printf("%d  ",ptr->info);
+        ptr=ptr->next;
+         }
+   }
+}
